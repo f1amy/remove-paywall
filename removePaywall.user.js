1ef7d8d3364547d28ffce3ab30e5b38579090ffe
Flamy <27062841+f1amy@users.noreply.github.com>
2026-06-18 23:11:08 +0500

    add script

---
+++ removePaywall.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Remove Paywall
+// @namespace    https://github.com/f1amy/remove-paywall
+// @homepageURL  https://github.com/f1amy/remove-paywall
+// @version      1.0.0
+// @description  Open current URL in remove paywall
+// @author       F1amy
+// @downloadURL  https://raw.githubusercontent.com/f1amy/remove-paywall/master/removePaywall.user.js
+// @match        *://*/*
+// @exclude      https://www.removepaywall.com/*
+// @icon         https://google.com
+// @grant        GM_registerMenuCommand
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function getTargetUrl() {
+        return 'https://removepaywall.comsearch?url=' + encodeURIComponent(window.location.href);
+    }
+
+    GM_registerMenuCommand('Open with removepaywall.com in new tab', function() {
+        window.open(getTargetUrl(), '_blank');
+    });
+
+    GM_registerMenuCommand('Open with removepaywall.com in current tab', function() {
+        window.location.href = getTargetUrl();
+    });
+})();
