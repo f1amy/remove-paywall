57e1c2ebb857292b457d63285b9b42e292cc2c50
Flamy <27062841+f1amy@users.noreply.github.com>
2026-06-19 00:18:35 +0500

    Add another site

--- removePaywall.user.js
+++ removePaywall.user.js
@@ -2,7 +2,7 @@
 // @name         Remove Paywall
 // @namespace    https://github.com/f1amy/remove-paywall
 // @homepageURL  https://github.com/f1amy/remove-paywall
-// @version      1.1.0
+// @version      1.2.0
 // @description  Open current URL in remove paywall
 // @author       F1amy
 // @downloadURL  https://raw.githubusercontent.com/f1amy/remove-paywall/main/removePaywall.user.js
@@ -15,15 +15,27 @@
 (function() {
     'use strict';
 
-    function getTargetUrl() {
+    function getTargetUrlRemovePaywall() {
         return 'https://www.removepaywall.com/search?url=' + encodeURIComponent(window.location.href);
     }
 
+    function getTargetUrlRemovePaywalls() {
+        return 'https://removepaywalls.com/' + encodeURIComponent(window.location.href);
+    }
+
     GM_registerMenuCommand('Open with removepaywall.com in new tab', function() {
-        window.open(getTargetUrl(), '_blank');
+        window.open(getTargetUrlRemovePaywall(), '_blank');
     });
 
     GM_registerMenuCommand('Open with removepaywall.com in current tab', function() {
-        window.location.href = getTargetUrl();
+        window.location.href = getTargetUrlRemovePaywall();
+    });
+
+    GM_registerMenuCommand('Open with removepaywalls.com in new tab', function() {
+        window.open(getTargetUrlRemovePaywalls(), '_blank');
+    });
+
+    GM_registerMenuCommand('Open with removepaywalls.com in current tab', function() {
+        window.location.href = getTargetUrlRemovePaywalls();
     });
 })();
