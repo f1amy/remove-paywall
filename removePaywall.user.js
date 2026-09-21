d16b131f9bbcb95f104c51eaa6ec1dfc5f669a20
Flamy <27062841+f1amy@users.noreply.github.com>
2026-09-21 12:11:07 +0500

    website removed

--- removePaywall.user.js
+++ removePaywall.user.js
@@ -2,7 +2,7 @@
 // @name         Remove Paywall
 // @namespace    https://github.com/f1amy/remove-paywall
 // @homepageURL  https://github.com/f1amy/remove-paywall
-// @version      1.2.3
+// @version      1.3.0
 // @description  Open current URL in remove paywall
 // @author       F1amy
 // @downloadURL  https://raw.githubusercontent.com/f1amy/remove-paywall/main/removePaywall.user.js
@@ -19,18 +19,6 @@
     function getTargetUrlRemovePaywall() {
         return 'https://www.removepaywall.com/search?url=' + encodeURIComponent(window.location.href);
     }
-
-    function getTargetUrlRemovePaywalls() {
-        return 'https://removepaywalls.com/' + encodeURIComponent(window.location.href);
-    }
-
-    GM_registerMenuCommand('Open with removepaywalls.com in new tab', function() {
-        window.open(getTargetUrlRemovePaywalls(), '_blank');
-    });
-
-    GM_registerMenuCommand('Open with removepaywalls.com in current tab', function() {
-        window.location.href = getTargetUrlRemovePaywalls();
-    });
 
     GM_registerMenuCommand('Open with removepaywall.com in new tab', function() {
         window.open(getTargetUrlRemovePaywall(), '_blank');
